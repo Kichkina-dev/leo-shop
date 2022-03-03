238718d407103dede18e328d7d0d80976f2eca9b
Kichkina dev <95954469+Kichkina-dev@users.noreply.github.com>
2022-03-03 16:43:45 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function navbar() {
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("navbar-off").style.display = "flex";
+    document.getElementById("menu").style.display = "flex";
+}
+function navbaroff() {
+    document.getElementById("navbar").style.display = "flex";
+    document.getElementById("navbar-off").style.display = "none";
+    document.getElementById("menu").style.display = "none";
+}
